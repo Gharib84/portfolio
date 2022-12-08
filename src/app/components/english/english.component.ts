fba6a4d813673e6588f74511865fce72e3f8cd71
gharib84 <itprofessional63@gmail.com>
2022-12-08 19:00:47 +0100

    showcase has created

--- src/app/components/english/english.component.ts
+++ src/app/components/english/english.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainNavComponent } from '../main.nav/main.nav.component';
-
+import { PresentationComponent } from '../presentation/presentation.component';
 @Component({
   selector: 'app-english',
   standalone: true,
-  imports: [CommonModule, MainNavComponent],
+  imports: [CommonModule, MainNavComponent, PresentationComponent],
   templateUrl: './english.component.html',
   styleUrls: ['./english.component.scss']
 })
