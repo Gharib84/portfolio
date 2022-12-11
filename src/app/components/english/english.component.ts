093b37f2122bece998639cd5ec9c3c65f6507053
gharib84 <itprofessional63@gmail.com>
2022-12-11 19:09:35 +0100

    headers has been finished

--- src/app/components/english/english.component.ts
+++ src/app/components/english/english.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainNavComponent } from '../main.nav/main.nav.component';
 import { PresentationComponent } from '../presentation/presentation.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-english',
   standalone: true,
-  imports: [CommonModule, MainNavComponent, PresentationComponent],
+  imports: [CommonModule, MainNavComponent, PresentationComponent, RouterModule],
   templateUrl: './english.component.html',
   styleUrls: ['./english.component.scss']
 })
---
+++ src/app/components/polish/polish.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-polish',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './polish.component.html',
+  styleUrls: ['./polish.component.scss']
+})
+export class PolishComponent {
+
+}
