0bc074fa949f83eb84487886050f45ea764c0e4c
Gharib84e <ITprofessional63@gmail.com>
2024-11-25 18:29:08 +0100

    portfolio updated

--- src/app/components/cards/card.component.ts
+++ src/app/components/cards/card.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, OnInit, AfterViewInit,AfterViewChecked } from '@angular/core';
+import { ExperienceComponent } from '../experience.component';
+import { CommonModule } from '@angular/common';
+import { Experience } from 'src/app/interfaces/experience';
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ExperienceComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
+  @ViewChild(ExperienceComponent, { static: false }) exp!: ExperienceComponent;
+  experiences:Experience[] = [];
+
+  public ngOnInit(): void {
+  }
+
+  public ngAfterViewInit(): void {
+    //this.getExperieces();
+  }
+
+  public ngAfterViewChecked(): void {
+    if (this.exp && !this.experiences.length) {
+      this.getExperieces();
+    }
+  }
+  
+
+  public getExperieces(): void {
+    if(this.exp){
+      this.experiences = this.exp.getDetails();
+    } else
+    {
+      console.log('undefined');
+    }
+  }
 
 }
--- src/app/components/presentation/presentation.component.ts
+++ src/app/components/presentation/presentation.component.ts
@@ -4,11 +4,11 @@ import { RouterModule } from '@angular/router';
 import { ProjectsComponent } from '../projects/projects.component';
 import { ButtonsComponent } from '../buttons.component';
 import { ExperienceComponent } from '../experience.component';
-
+import { CardComponent } from '../cards/card.component';
 @Component({
   selector: 'app-presentation',
   standalone: true,
-  imports: [CommonModule, RouterModule, ProjectsComponent,ButtonsComponent,ExperienceComponent],
+  imports: [CommonModule, RouterModule, ProjectsComponent,ButtonsComponent,ExperienceComponent,CardComponent],
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss']
 })
