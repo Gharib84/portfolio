519b2b4d0f17d47f4ddcf185555a0404d3ddb525
Gharib84e <ITprofessional63@gmail.com>
2024-12-21 23:15:01 +0100

    gsap integrated with main project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     standalone: false
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger) 
+  }
 }
