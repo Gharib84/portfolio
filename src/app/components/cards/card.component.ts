3e01e2df5c128f7bea3b3908aa61f74973d79cc2
Gharib84e <ITprofessional63@gmail.com>
2025-02-03 21:25:39 +0100

    experience section updated

--- src/app/components/cards/card.component.ts
+++ src/app/components/cards/card.component.ts
@@ -10,7 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { TranslatePipe } from '@ngx-translate/core';
 @Component({
   selector: 'app-card',
-  imports: [CommonModule, FontAwesomeModule, FadeInDirective,TranslatePipe],
+  imports: [CommonModule, FontAwesomeModule, FadeInDirective, TranslatePipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
@@ -22,21 +22,22 @@ export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
   faCheck = faCheck;
   faPersonRunning = faPersonRunning;
   public works: Experience[] = [
-    {
-      company: "INTRO.WORK.EKO_OKNA.COMPANY",
-      type: "Hybrid",
-      title: "INTRO.WORK.EKO_OKNA.TITLE.JOB",
-      start: "INTRO.WORK.EKO_OKNA.PERIOD.START",
-      end:   "INTRO.WORK.EKO_OKNA.PERIOD.END",
-      tasks: []
-    },
+
     {
       platform: "INTRO.WORK.FREELANCER.platform",
       type: "Remote",
       title: "INTRO.WORK.FREELANCER.TITLE.JOB",
       start: "INTRO.WORK.FREELANCER.PERIOD.START",
       tasks: []
-    }
+    },
+    {
+      company: "INTRO.WORK.EKO_OKNA.COMPANY",
+      type: "Hybrid",
+      title: "INTRO.WORK.EKO_OKNA.TITLE.JOB",
+      start: "INTRO.WORK.EKO_OKNA.PERIOD.START",
+      end: "INTRO.WORK.EKO_OKNA.PERIOD.END",
+      tasks: []
+    },
   ]
   private translate = inject(TranslateService);
   currentLanguage: string = 'english'; // Default language
@@ -51,14 +52,14 @@ export class CardComponent implements OnInit, AfterViewInit, AfterViewChecked {
   getTranslatedTasks(work: Experience): string[] {
     const key = this.getCompanyKey(work); // Determine company key
     let tasks: string[] = [];
-    
+
     this.translate.get(`INTRO.WORK.${key}.TASKS`).subscribe((translatedTasks: string[]) => {
       tasks = translatedTasks;
     });
-  
+
     return tasks;
   }
-  
+
 
   public ngAfterViewInit(): void {
   }
