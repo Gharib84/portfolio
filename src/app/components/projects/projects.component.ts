fb43eeaf3b521e45911c1301b2d000ef778fb54a
Gharib84e <ITprofessional63@gmail.com>
2025-04-03 20:57:32 +0200

    project dependenceis updated

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -8,6 +8,8 @@ import * as THREE from 'three';
 import { WebGLRenderer } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { AnimationMixer } from 'three';
+import { step, normalWorld, output, texture, vec3, vec4, normalize, positionWorld, bumpMap, cameraPosition, color, uniform, mix, uv, max } from 'three/build/three.tsl';
 @Component({
     selector: 'app-projects',
     imports: [CommonModule,TranslatePipe,FadeInDirective],
